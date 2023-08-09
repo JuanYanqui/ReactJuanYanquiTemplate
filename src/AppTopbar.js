e03996a0d3a604fdbdc06bfa9d2e87e96e1bffd3
JuanYanqui <juan.yanqui.est@tecazuay.edu.ec>
2023-08-09 16:35:53 -0500

    cambios
    cambios

--- src/AppTopbar.js
+++ src/AppTopbar.js
@@ -67,7 +67,7 @@ const AppTopbar = (props) => {
     return (
         <div className="layout-topbar shadow-4" style={{ backgroundColor: '#3e464c', height: '5.5rem' }}>
             <div className="layout-topbar-left" style={{ height: '5.5rem' }}>
-                <button type="button" style={{ cursor: 'pointer', background: '#2b3135' ,height: '5.5rem' }} className="layout-topbar-logo p-link" onClick={() => navigate('/')}>
+                <button type="button" style={{ cursor: 'pointer', background: '#2b3135', height: '5.5rem' }} className="layout-topbar-logo p-link" onClick={() => navigate('/')}>
                     <img id="app-logo" src="assets/layout/images/web_logo_header.png" alt="ultima-layout" style={{ height: '2rem' }} />
                 </button>
                 <button type="button" className="layout-menu-button shadow-6 p-link" onClick={props.onMenuButtonClick} style={botonEstilo2}>
@@ -80,49 +80,37 @@ const AppTopbar = (props) => {
             </div>
 
             <div className={classNames('layout-topbar-right', { 'layout-topbar-mobile-active': props.mobileTopbarActive })}>
-                <div className="layout-topbar-actions-left">
-
-                </div>
-                <div className="layout-topbar-actions-right">
-                    <ul className="layout-topbar-items">
-                        <li className="layout-topbar-item notifications">
-                            <div className="dropdown-container">
-                            <button className="dropdown-toggle" onClick={toggleMenu} style={{ background: 'transparent', border: 'none', padding: '0' }}>
-    <i className="pi pi-cog p-button-icon p-link" style={{ fontSize: '35px' }}></i>
-</button>
-                                {isMenuOpen && (
-                                    <ul ref={inlineMenuRef} className="layout-inline-menu-action-panel custom-dropdown-panel">
-                                        <li className="layout-inline-menu-action-item tooltip" data-pr-tooltip="Settings">
-                                            <button className="flex flex-row align-items-center p-link">
-                                                <i className="pi pi-cog pi-fw"></i>
-                                                <span>Settings</span>
-                                            </button>
-                                        </li>
-                                        <li className="layout-inline-menu-action-item tooltip" data-pr-tooltip="Terms of Usage">
-                                            <button className="flex flex-row align-items-center p-link">
-                                                <i className="pi pi-file pi-fw"></i>
-                                                <span>Terms of Usage</span>
-                                            </button>
-                                        </li>
-                                        <li className="layout-inline-menu-action-item tooltip" data-pr-tooltip="Support">
-                                            <button className="flex flex-row align-items-center p-link">
-                                                <i className="pi pi-compass pi-fw"></i>
-                                                <span>Support</span>
-                                            </button>
-                                        </li>
-                                        <li className="layout-inline-menu-action-item tooltip" data-pr-tooltip="Logout">
-                                            <button className="flex flex-row align-items-center p-link" onClick={handleLogout}>
-                                                <i className="pi pi-power-off pi-fw"></i>
-                                                <span>Logout</span>
-                                            </button>
-                                        </li>
-                                    </ul>
-                                )}
-                            </div>
-                        </li>
+    <div className="layout-topbar-actions-left"></div>
+    <div className="layout-topbar-actions-right">
+        <ul className="layout-topbar-items">
+            <li className="layout-topbar-item notifications">
+                <div className="dropdown-container">
+                    <button className="dropdown-toggle" onClick={toggleMenu} style={{ background: 'transparent', border: 'none', padding: '0' }}>
+                        <i className="pi pi-cog p-button-icon p-link" style={botonEstilo}></i>
+                    </button>
+                    {isMenuOpen && (
+                        <ul ref={inlineMenuRef} className="layout-inline-menu-action-panel custom-dropdown-panel">
+                        {[
+                            { icon: 'pi-cog', label: 'Settings' },
+                            { icon: 'pi-compass', label: 'Support' },
+                            { icon: 'pi-power-off', label: 'Logout', onClick: handleLogout },
+                        ].map((item, index) => (
+                            <li className="layout-inline-menu-action-item tooltip" data-pr-tooltip={item.label} key={index}>
+                                <button className="flex flex-row align-items-center p-link" onClick={item.onClick}>
+                                    <div className="menu-item-box">
+                                        <i className={`pi ${item.icon} pi-fw`}></i>
+                                        <span>{item.label}</span>
+                                    </div>
+                                </button>
+                            </li>
+                        ))}
                     </ul>
+                    )}
                 </div>
-            </div>
+            </li>
+        </ul>
+    </div>
+</div>
         </div>
     );
 };
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,122 +1,299 @@
-import React, { useRef, useState, useEffect, useContext } from 'react';
-import { classNames } from 'primereact/utils';
-import { Button } from 'primereact/button';
-import { Chart } from 'primereact/chart';
-import { ProgressBar } from 'primereact/progressbar';
-import { Menu } from 'primereact/menu';
-import { Card } from 'primereact/card';
-import { ProductService } from '../service/ProductService';
-import { Column } from 'primereact/column';
+
+import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
-import { Timeline } from 'primereact/timeline';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
-import { OverlayPanel } from 'primereact/overlaypanel';
-import { RTLContext } from '../App';
-import { TreeTable } from 'primereact/treetable';
-
-const Dashboard = (props) => {
-    const [nodes, setNodes] = useState([]);
-    const [first, setFirst] = useState(0);
-    const [rows, setRows] = useState(10);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-            setNodes(loadNodes(first, rows));
-            setTotalRecords(1000);
-        }, 500);
-    }, []);
-
-    const loadNodes = (first, rows) => {
-        let nodes = [];
-
-        for (let i = 0; i < rows; i++) {
-            let node = {
-                key: first + i,
-                data: {
-                    name: 'Item ' + (first + i),
-                    size: Math.floor(Math.random() * 1000) + 1 + 'kb',
-                    type: 'Type ' + (first + i)
-                },
-                leaf: false
-            };
-
-            nodes.push(node);
-        }
-
-        return nodes;
-    };
-
-    const onExpand = (event) => {
-        if (!event.node.children) {
-            setLoading(true);
-
-            setTimeout(() => {
-                setLoading(false);
-                let lazyNode = { ...event.node };
-
-                lazyNode.children = [
+import { Toolbar } from 'primereact/toolbar';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { RadioButton } from 'primereact/radiobutton';
+import { InputNumber } from 'primereact/inputnumber';
+import { Dialog } from 'primereact/dialog';
+import { Dropdown } from 'primereact/dropdown';
+export default function LazyLoadDemo() {
+    const [isDialogVisible, setDialogVisible] = useState(false);
+    const [expandedRows, setExpandedRows] = useState([]);
+    const [dropdownItem, setDropdownItem] = useState(null);
+    const [searchText, setSearchText] = useState("");
+    const dropdownItems = [
+        { name: 'Option 1', code: 'Option 1' },
+        { name: 'Option 2', code: 'Option 2' },
+        { name: 'Option 3', code: 'Option 3' }
+    ];
+    // Inside your component
+    const userData = {
+        object: [
+            {
+                menId: 652,
+                nombre: "Coral Seccion",
+                url: "https://www.gerardoortiz.com/coralSeccion/",
+                icono: "fa fa-tags",
+                hijos: [
                     {
-                        data: {
-                            name: lazyNode.data.name + ' - 0',
-                            size: Math.floor(Math.random() * 1000) + 1 + 'kb',
-                            type: 'File'
-                        }
+                        menId: 653,
+                        nombre: "Reporte Factura TEST",
+                        url: "https://www.gerardoortiz.com/coralSeccion/reportes/factura_codigo_cliente.jsf",
+                        icono: "fa fa-crosshairs"
                     },
                     {
-                        data: {
-                            name: lazyNode.data.name + ' - 1',
-                            size: Math.floor(Math.random() * 1000) + 1 + 'kb',
-                            type: 'File'
-                        }
+                        menId: 658,
+                        nombre: "Precio historico de articulo",
+                        url: "https://www.gerardoortiz.com/coralSeccion/reportes/ingresos_historicos.jsf",
+                        icono: "fa fa-dollar"
                     }
-                ];
-
-                let _nodes = nodes.map((node) => {
-                    if (node.key === event.node.key) {
-                        node = lazyNode;
+                ]
+            },
+            {
+                menId: 805,
+                nombre: "Activos Fijos",
+                url: "#",
+                icono: "fas fa-book",
+                hijos: [
+                    {
+                        menId: 806,
+                        nombre: "Formar Activo",
+                        url: "https://www.gerardoortiz.com/activosFijos/activosFijos/formaActivo.jsf",
+                        icono: "fas fa-clipboard-list"
+                    },
+                    {
+                        menId: 807,
+                        nombre: "Ingresar Activo",
+                        url: "https://www.gerardoortiz.com/activosFijos/activosFijos/ingresaActivo.jsf",
+                        icono: "fas fa-clipboard-list"
+                    },
+                    {
+                        menId: 808,
+                        nombre: "Recepcion Entrega",
+                        url: "https://www.gerardoortiz.com/activosFijos/activosFijos/recepcionEntrega.jsf",
+                        icono: "fas fa-clipboard-list"
+                    },
+                    {
+                        menId: 809,
+                        nombre: "Depreciacion del Activo",
+                        url: "https://www.gerardoortiz.com/activosFijos/activosFijos/depreciaActivo.jsf",
+                        icono: "fas fa-clipboard-list"
+                    },
+                    {
+                        menId: 810,
+                        nombre: "Gestion Sitio",
+                        url: "https://www.gerardoortiz.com/activosFijos/activosFijos/gestionSitio.jsf",
+                        icono: "fas fa-clipboard-list"
                     }
+                ]
+            },
+            {
+                menId: 288,
+                nombre: "Regalos",
+                url: "#",
+                icono: "fas fa-book",
+                hijos: [
+                    {
+                        menId: 107,
+                        nombre: "Regalos Activos",
+                        url: "https://www.gerardoortiz.com/activosFijos/activosFijos/formaActivo.jsf",
+                        icono: "fas fa-clipboard-list"
+                    },
+                ]
+            }
 
-                    return node;
-                });
+        ]
+    };
 
-                setLoading(false);
-                setNodes(_nodes);
-            }, 250);
+    const preparedData = [];
+    userData.object.forEach((parentItem) => {
+        const parentRow = {
+            id: parentItem.menId,
+            name: parentItem.nombre,
+            url: parentItem.url,
+            icon: parentItem.icono,
+            children: parentItem.hijos || [],
+        };
+
+        preparedData.push(parentRow);
+
+        if (parentItem.hijos) {
+            parentItem.hijos.forEach((childItem) => {
+                const childRow = {
+                    id: childItem.menId,
+                    name: childItem.nombre,
+                    url: childItem.url,
+                    icon: childItem.icono,
+                    parent: parentItem.menId,
+                };
+                preparedData.push(childRow);
+            });
+        }
+    });
+    const [activeSubMenuIndex, setActiveSubMenuIndex] = useState(null);
+
+    const toggleRow = (rowData) => {
+        const isRowExpanded = expandedRows.includes(rowData.id);
+        if (isRowExpanded) {
+            setExpandedRows(expandedRows.filter(id => id !== rowData.id));
+        } else {
+            setExpandedRows([...expandedRows, rowData.id]);
         }
     };
 
-    const onPage = (event) => {
-        setLoading(true);
-
-        setTimeout(() => {
-            setFirst(event.first);
-            setRows(event.rows);
-            setNodes(loadNodes(event.first, event.rows));
-            setLoading(false);
-        }, 500);
+    const isSubMenuActive = (menId) => {
+        return activeSubMenuIndex === menId;
     };
 
-    return (
-        <div className="card">
-            <TreeTable value={nodes} lazy paginator totalRecords={totalRecords}
-                first={first} rows={rows} onPage={onPage} onExpand={onExpand} loading={loading} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="name" header="Name" expander></Column>
-                <Column field="size" header="Size"></Column>
-                <Column field="type" header="Type"></Column>
-            </TreeTable>
+    const onSubMenuClick = (menId) => {
+        setActiveSubMenuIndex((prevActiveIndex) => (prevActiveIndex === menId ? null : menId));
+    };
+
+    const rowExpansionTemplate = (rowData) => {
+        if (!rowData.children || rowData.children.length === 0) return null;
+    
+        return (
+            <div className="p-mb-4">
+                {rowData.children.map((child, index) => (
+                    <div key={child.id} className="p-d-flex p-jc-between p-ai-center">
+                        <a href={child.url}>{child.name}</a>
+                        <i className={child.icon}></i>
+                        {child.hijos && child.hijos.length > 0 && (
+                            <Button
+                                icon={`pi ${isSubMenuActive(child.menId) ? 'pi-angle-up' : 'pi-angle-down'}`}
+                                className={`p-row-toggler p-link`}
+                                onClick={() => onSubMenuClick(child.menId)}
+                                aria-expanded={isSubMenuActive(child.menId)}
+                            />
+                        )}
+                    </div>
+                ))}
+            </div>
+        );
+    };
+    
+    const openNew = () => {
+        setDialogVisible(true);
+    };
+
+    const hideDialog = () => {
+        setDialogVisible(false);
+    };
+    const leftToolbarTemplate = () => {
+        return (
+            <div className="flex flex-wrap gap-2">
+                <Button label="New" icon="pi pi-plus" severity="success" onClick={openNew} />
+                <Button label="Delete" icon="pi pi-trash" severity="danger" />
+            </div>
+        );
+    };
+    const rightToolbarTemplate = () => {
+        return <Button label="Export" icon="pi pi-upload" className="p-button-help" />;
+    };
+    const header = (
+        <div className="flex flex-wrap gap-2 align-items-center justify-content-between">
+            <h4 className="m-0">Prueba</h4>
+            <span className="p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText value={searchText} onChange={(e) => setSearchText(e.target.value)} placeholder="Search..." />
+            </span>
         </div>
     );
-};
 
-const comparisonFn = function (prevProps, nextProps) {
-    return prevProps.location.pathname === nextProps.location.pathname && prevProps.colorMode === nextProps.colorMode && prevProps.isNewThemeLoaded === nextProps.isNewThemeLoaded && prevProps.onNewThemeChange === nextProps.onNewThemeChange;
-};
+    const filteredData = preparedData.filter(
+        (row) =>
+          row.name.toLowerCase().includes(searchText.toLowerCase()) ||
+          (row.url && row.url.toLowerCase().includes(searchText.toLowerCase()))
+      );
 
-export default React.memo(Dashboard, comparisonFn);
+    const productDialogFooter = (
+        <React.Fragment>
+            <Button label="Cancel" icon="pi pi-times" onClick={hideDialog} />
+            <Button label="Save" icon="pi pi-check" />
+        </React.Fragment>
+    );
+    const deleteProductDialogFooter = (
+        <React.Fragment>
+            <Button label="No" icon="pi pi-times" />
+            <Button label="Yes" icon="pi pi-check" severity="danger" />
+        </React.Fragment>
+    );
+    const deleteProductsDialogFooter = (
+        <React.Fragment>
+            <Button label="No" icon="pi pi-times" outlined />
+            <Button label="Yes" icon="pi pi-check" severity="danger" />
+        </React.Fragment>
+    );
+
+    return (
+        <div>
+            <div className="card">
+                <Toolbar className="mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
+                <DataTable
+                value={filteredData}
+                dataKey="id"
+                rowExpansionTemplate={rowExpansionTemplate}
+                paginator
+                rows={2}
+                rowsPerPageOptions={[5, 10, 25]}
+                expandedRows={expandedRows}
+                onRowToggle={(e) => toggleRow(e.data)} 
+                header={header} 
+            >
+                <Column
+                    expander
+                    style={{ width: '3em' }}
+                    body={(rowData) =>
+                        rowData.children && rowData.children.length > 0 ? (
+                            <Button
+                                icon="pi pi-angle-down"
+                                className={`p-row-toggler p-link`}
+                                onClick={() => toggleRow(rowData)}
+                                aria-expanded={expandedRows.includes(rowData.id)}
+                            />
+                        ) : null
+                    }
+                />
+                <Column selectionMode="multiple" exportable={false}></Column>
+                <Column field="icon" header="" body={(rowData) => <i className={rowData.icon}></i>} style={{ textAlign: 'center' }} />
+                <Column field="name" header="Name" sortable filter filterPlaceholder="Search" />
+                <Column field="url" header="URL" body={(rowData) => <a href={rowData.url}>{rowData.url}</a>} />
+            </DataTable>
+            </div>
+            <Dialog visible={isDialogVisible} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Detalle" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
+            <div className="grid">
 
 
+            <div className="col-12">
+                <div className="card">
+                    <h5>Advanced</h5>
+                    <div className="p-fluid formgrid grid">
+                        <div className="field col-12 md:col-6">
+                            <label htmlFor="firstname2">Firstname</label>
+                            <InputText id="firstname2" type="text" />
+                        </div>
+                        <div className="field col-12 md:col-6">
+                            <label htmlFor="lastname2">Lastname</label>
+                            <InputText id="lastname2" type="text" />
+                        </div>
+                        <div className="field col-12">
+                            <label htmlFor="address">Address</label>
+                            <InputTextarea id="address" rows="4" />
+                        </div>
+                        <div className="field col-12 md:col-6">
+                            <label htmlFor="city">City</label>
+                            <InputText id="city" type="text" />
+                        </div>
+                        <div className="field col-12 md:col-3">
+                            <label htmlFor="state">State</label>
+                            <Dropdown id="state" value={dropdownItem} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown>
+                        </div>
+                        <div className="field col-12 md:col-3">
+                            <label htmlFor="zip">Zip</label>
+                            <InputText id="zip" type="text" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            </Dialog>
+
+        </div>
+    );
 
 
+}
